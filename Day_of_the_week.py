15087bccb7ce41e667897e7b4928d8ab1bd42ccf
ebi <ebi.mohamadi63@gmail.com>
2023-01-18 12:05:39 +0000

    new projects

---
+++ Day_of_the_week.py
@@ -0,0 +1,7 @@
+Day = ["Saturday","Sunday","Monday","Tuesday","Wednesday","Thersday","Friday"]
+
+count =0 
+
+while count <= 6:
+    print("Today is: ", Day[count])
+    count = count + 1
---
+++ While_sample.py
@@ -0,0 +1,6 @@
+
+num = 20
+
+while num > 0:
+    print(num)
+    num -= 1
